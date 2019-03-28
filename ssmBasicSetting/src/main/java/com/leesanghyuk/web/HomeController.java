6a06d17096215d8035029d2cca87523f1019d182
zhang chang <1064174415@qq.com>
2019-03-28 16:17:33 +0800

    1064174415@qq.com

--- ssmBasicSetting/src/main/java/com/leesanghyuk/web/HomeController.java
+++ ssmBasicSetting/src/main/java/com/leesanghyuk/web/HomeController.java
@@ -1,11 +1,7 @@
 package com.leesanghyuk.web;
 
 
-import com.leesanghyuk.mapper.MusicMapper;
 import com.leesanghyuk.model.Music;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,14 +10,25 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping(value={"/","/homepage"})
 public class HomeController {
-    @Autowired
-    private MusicMapper musicMapper_auto;
+//    @Autowired
+//    private MusicMapper musicMapper_auto;
 
     @RequestMapping(method = RequestMethod.GET)
     public String home(Model model){
 
-        Music music1=musicMapper_auto.selectMusicById(2);
-        model.addAttribute("message","Bounjour Mondo!"+music1.getTitle());
+//        Music music1=musicMapper_auto.selectMusicById(2);
+//        model.addAttribute("message","Bounjour Mondo!"+music1.getTitle());
+        model.addAttribute("name", "zhang" );
         return "index";
+    }
+    @RequestMapping("/test")
+    public String test(Model l) {
+//        l.addAttribute("sex", "man");
+        Music m = new Music();
+//        Apple a = new Apple();
+        m.setId("a");
+        m.setTitle("b");
+        l.addAttribute("music", m);
+        return "experimentBookingPage";
     }
 }
